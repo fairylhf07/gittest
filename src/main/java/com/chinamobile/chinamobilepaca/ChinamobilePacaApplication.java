3af9169747a4d831f6d390a5e27f5c45d30c626f
zhaolei <zhaoleids@chinamobile.com>
2020-11-12 15:45:46 +0800

    测试1111

--- src/main/java/com/chinamobile/chinamobilepaca/ChinamobilePacaApplication.java
+++ src/main/java/com/chinamobile/chinamobilepaca/ChinamobilePacaApplication.java
@@ -10,6 +10,7 @@ public class ChinamobilePacaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChinamobilePacaApplication.class, args);
+        System.out.println("git测试1111111111111111111111111");
     }
 
 }

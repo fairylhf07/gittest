ca48bebf8ae97977e0043f5901a023c942424a27
zhaolei <zhaoleids@chinamobile.com>
2020-11-12 15:24:58 +0800

    hehe

---
+++ src/main/java/com/chinamobile/chinamobilepaca/ChinamobilePacaApplication.java
@@ -0,0 +1,15 @@
+package com.chinamobile.chinamobilepaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class ChinamobilePacaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChinamobilePacaApplication.class, args);
+    }
+
+}
